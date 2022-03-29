61188594cc4d0763c763f8e48bc3abc3afd0a28e
msamm <msamm93@gmail.com>
2022-03-28 23:01:02 -0500

    hardcode display issues for a repo

---
+++ assets/js/single.js
@@ -0,0 +1,59 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var apiUrl = "https://api.github.com/repos/facebook/react/issues?direction=asc";
+
+var displayIssues = function(issues) {
+    // Check if there are not issues and show a message if there aren't
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues";
+        return;
+    }
+    
+    for (i = 0; i < issues.length; i++) {
+        // create a link element to take users to teh issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to issueEl
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        }
+        else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append type to issue
+        issueEl.appendChild(typeEl);
+
+        // append issues to container
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+var getRepoIssues = function(repo) {
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayIssues(data);
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+
+
+getRepoIssues("facebook/react");
